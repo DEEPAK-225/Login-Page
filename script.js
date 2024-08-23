f096e1183bae1511caf55daca726d04cc14cfb36
DEEPAK-225 <kumarvermadeepak225@gmail.com>
2024-08-23 12:26:43 +0530

    Login page with toggle functionality of Sign In and Sign Up

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ const loginBtn = document.getElementById('login');
 registerBtn.addEventListener('click', ()=>{
   container.classList.add("active");
 });
-
 loginBtn.addEventListener('click', ()=>{
-  container.classList.add("active");
+  container.classList.remove("active");
 });
